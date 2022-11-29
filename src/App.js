53e53dce29587680b166db5ec8abb7df4900ebb9
Seokmin <newacon@naver.com>
2022-11-29 20:40:17 +0200

    React useEffect + 2way function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-
+function Hello() {
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
 
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
