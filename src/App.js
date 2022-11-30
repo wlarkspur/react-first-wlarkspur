2dff40b31614519c7496f7804f3d8b9a49434baa
Seokmin <newacon@naver.com>
2022-12-01 01:35:44 +0200

    coin tracker wo symbol

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState();
-  const onChange = (item) => {
-    setUsd(item / coins.quotes.USD.price);
+  const [usd, setUsd] = useState(0);
+  const [price, setPrice] = useState(1);
+
+  const inputUsd = (event) => {
+    setUsd(event.target.value);
   };
+  const onChange = (event) => {
+    setPrice(event.target.value);
+  };
+  const finalValue = usd / price;
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -15,27 +21,35 @@ function App() {
         setLoading(false);
       });
   }, []);
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <form>
-        <input onChange={onChange} type="number" />
-        <button></button>
-      </form>
-
+      <input value={usd} type="number" onChange={inputUsd} placeholder="USD" />
+      USD
+      <h1>
+        {finalValue.toFixed(8)}
+        {}
+      </h1>
       <br />
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onChange}>
+          <option>Select Coin</option>
           {coins.map((coin) => (
-            <option>
+            <option
+              key={coin.id}
+              value={coin.quotes.USD.price}
+              symbol={coin.symbol}
+            >
               {coin.name}({coin.symbol}) : $
               {Math.round(coin.quotes.USD.price * 100) / 100} USD
             </option>
           ))}
         </select>
       )}
+      <br />
     </div>
   );
 }
