98e3be3ea8bb22cb3e9a8cc596b059d42ec3e116
Seokmin <newacon@naver.com>
2022-11-30 23:45:17 +0200

    React cointicker in process...

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState();
+  const onChange = (item) => {
+    setUsd(item / coins.quotes.USD.price);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <form>
+        <input onChange={onChange} type="number" />
+        <button></button>
+      </form>
+
+      <br />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name}({coin.symbol}) : $
+              {Math.round(coin.quotes.USD.price * 100) / 100} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
