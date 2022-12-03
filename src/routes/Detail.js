38ecc62edd2f0dd275a6c9aac77f8113eb929524
Seokmin <newacon@naver.com>
2022-12-04 01:59:03 +0200

    MOD: state value added

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,33 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { redirect, useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
+  const [movieDetail, setMovieDetail] = useState({});
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+
+    setMovieDetail(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  console.log(movieDetail);
+  return (
+    <div>
+      <h2>{movieDetail.title}</h2>
+      <img src={movieDetail.medium_cover_image} alt="Cover img" />
+      <h2>Year: {movieDetail.year}</h2>
+      <p>rating: {movieDetail.rating}</p>
+      <h2>{movieDetail.genres}</h2>
+      <p>{movieDetail.description_full}</p>
+    </div>
+  );
 }
 
 export default Detail;
